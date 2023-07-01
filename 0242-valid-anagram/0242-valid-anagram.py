66c3e6ff9a12ce4fac039b9e7824243b324c3dce
vidafeng <71468356+vidafeng@users.noreply.github.com>
2023-07-01 00:20:35 -0400

    Time: 48 ms (47.16%), Space: 13.7 MB (94.97%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -13,13 +13,19 @@ class Solution(object):
 
         for char in s:
             hash[char] = hash.get(char, 0) + 1
+            # get method to increment the count of char if it alrdy exists, otherwise set the count to 1
         
         for char in t: 
             if char not in hash or hash[char] == 0:
                 return False
             else:
                 hash[char] -= 1
-                
+
         return True
+
+
+# time complexity: o(n) where n is the legnth of the longer string since we need to iterate over each character of both strings once
+
+# space is o(n) bc we use hash map to store frequency of char in the first string
 
 
