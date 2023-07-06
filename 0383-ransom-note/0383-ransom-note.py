bffc9f5eea9c70d196d29f18e4d5ef6718b84109
vidafeng <71468356+vidafeng@users.noreply.github.com>
2023-07-05 23:27:32 -0400

    Time: 74 ms (60.33%), Space: 16.8 MB (10.35%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -6,18 +6,33 @@ class Solution:
         # if letter is not in hash, return false 
         # return true
 
-        dict = {}
+        # dict = {}
 
-        for char in magazine:
-            if char not in dict:
-                dict[char] = 0
-            dict[char] += 1
+        # for char in magazine:
+        #     if char not in dict:
+        #         dict[char] = 0
+        #     dict[char] += 1
         
-        for char in ransomNote:
-            if char not in dict or dict[char] == 0:
-                return False
-            else:
-                dict[char] -= 1
+        # for char in ransomNote:
+        #     if char not in dict or dict[char] == 0:
+        #         return False
+        #     else:
+        #         dict[char] -= 1
             
-        return True
+        # return True
 
+        # iterate over each char in ransomNote str
+        # for each char, check if present in magazine string
+        # if char is found, remove char from magazine str
+        # remove method only on lists
+        # if not found, immediately return False
+        # return true if we have successfully removed each char from magazine str
+
+        magazine = list(magazine)
+
+        for char in ransomNote:
+            if char in magazine:
+                magazine.remove(char)
+            else:
+                return False
+        return True
