c2efebdc8e90ee9b941109ecb0e17cdb9ea11df1
vidafeng <71468356+vidafeng@users.noreply.github.com>
2023-07-15 23:49:30 -0400

    Time: 176 ms (57.73%), Space: 17.8 MB (73.68%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,5 +1,13 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
+# if element occurs more than n/2 times in the array
+# it will always occupy the middle position when array is sorted
+# sort the array and return the element at index n/2 
+
+
         nums.sort()
         n = len(nums)
+        # return nums[int(n/2)]
+        # floor division 
         return nums[n//2]
+
