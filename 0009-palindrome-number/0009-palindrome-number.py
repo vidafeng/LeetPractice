029cf124698943545870bb8313004431ba53d6c7
vidafeng <71468356+vidafeng@users.noreply.github.com>
2023-07-28 16:30:56 -0400

    Time: 60 ms (96.4%), Space: 16.5 MB (5.48%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,5 +1,10 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
+        # if x is neg, return false
+
+        if x < 0:
+            return False
+            
         x = str(x)
         i = 0
         j = len(x)-1
