6fdb120aaf18a532a8082cf78b3964c886eabd37
vidafeng <71468356+vidafeng@users.noreply.github.com>
2023-07-31 22:40:42 -0400

    Time: 57 ms (96.88%), Space: 20.2 MB (87.52%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        # tortoise and hare
+        # initalize fast and slow pointers
+            # fast = traverse two at a time
+            # slow = traverse one at a time
+            # if both pointers end up at the same node, cycle is true 
+
+        fast = head
+        while fast and fast.next:
+            head = head.next
+            fast = fast.next.next
+            if head is fast: 
+                return True
+        return False
+
+        
