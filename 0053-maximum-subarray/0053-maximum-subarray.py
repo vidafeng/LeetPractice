247e741a98c4211da5192bd73968b7fc18fe5a6a
vidafeng <71468356+vidafeng@users.noreply.github.com>
2023-08-01 23:58:56 -0400

    Time: 637 ms (93.17%), Space: 31.1 MB (8.73%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -20,10 +20,27 @@ class Solution:
         #         currentSum = 0
         
         # return maxSum
-        n = len(nums)
-        maximumSum, currSumSubarray = float('-inf'), 0
-        for i in range(n):
-            currSumSubarray += nums[i]
-            maximumSum = max(maximumSum, currSumSubarray)
-            currSumSubarray = max(currSumSubarray, 0)
-        return maximumSum
+        # n = len(nums)
+        # maximumSum, currSumSubarray = float('-inf'), 0
+        # for i in range(n):
+        #     currSumSubarray += nums[i]
+        #     maximumSum = max(maximumSum, currSumSubarray)
+        #     currSumSubarray = max(currSumSubarray, 0)
+        # return maximumSum
+
+        # Create an array...
+        arr = []
+        arr.append(nums[0])
+        # Initialize the max sum...
+        maxSum = arr[0]
+        # Traverse all the element through the loop...
+        for i in range(1, len(nums)):
+            # arr[i] represents the largest sum of all subarrays ending with index i...
+            # then its value should be the larger one between nums[i]...
+            # arr[i-1] + nums[i] (largest sum plus current number with using prefix)...
+            # calculate arr[0], arr[1]…, arr[n] while comparing each one with current largest sum...
+            arr.append(max(arr[i-1] + nums[i], nums[i]))
+            # if arr[i] > maxSum then maxSum = arr[i].
+            if arr[i] > maxSum:
+                maxSum = arr[i]
+        return maxSum       # Return the contiguous subarray which has the largest sum...
