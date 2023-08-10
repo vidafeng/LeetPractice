a6d845d19eb1cf64257558e6bc7c1ea48ed4e23e
vidafeng <71468356+vidafeng@users.noreply.github.com>
2023-08-09 23:24:49 -0400

    Time: 39 ms (98.79%), Space: 17.2 MB (40.48%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,10 +1,11 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         # convert all char in s to lowercase
-        # initialize empty string
+        # initialize empty result string
         # iterate over given string by converting to lowercase
         # check if corresponding value is character or num
-            # if it is, 
+            # if it is, add to result string
+        # check if result string is palidrone by slicing from end
 
         res = ''
         s = s.lower()
