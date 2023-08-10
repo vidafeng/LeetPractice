2e5216bd66051c2b3194766d5a983d2e8e8e7648
vidafeng <71468356+vidafeng@users.noreply.github.com>
2023-08-09 23:22:53 -0400

    Time: 47 ms (92.49%), Space: 17.1 MB (78.65%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,12 +1,16 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         # convert all char in s to lowercase
+        # initialize empty string
+        # iterate over given string by converting to lowercase
+        # check if corresponding value is character or num
+            # if it is, 
 
         res = ''
         s = s.lower()
 
         for char in s:
-            if char.isalnum() or char.isnumeric():
+            if char.isalnum():
                 res += char
         print(res)
         
