afbb122cec8fa95e9f9d6e974f495da5c63545fd
vidafeng <71468356+vidafeng@users.noreply.github.com>
2023-08-11 17:16:23 -0400

    Time: 33 ms (94.15%), Space: 16.4 MB (30.69%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,12 +1,13 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         # fibonacci series
+        # the next distinct way of climbing stairs is equal to the sum of the last two distinct ways
         # base cases are when we are on the 
             # 1st stair -- only one way to reach it
             # 2nd stair -- 2 ways to reach it
             # by summing up the number of ways to reach the n-1 and n-2 stairs
             # we can compute the total number of ways to climb the stairs
-
+        if n <=2: return n
 
 
         prev = 1
