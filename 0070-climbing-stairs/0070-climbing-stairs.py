e219ed72e8afc501262dbd7f2de4d7803ec4cb08
vidafeng <71468356+vidafeng@users.noreply.github.com>
2023-08-11 17:12:04 -0400

    Time: 43 ms (60.02%), Space: 16.2 MB (67.34%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,11 +1,19 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
+        # fibonacci series
+        # base cases are when we are on the 
+            # 1st stair -- only one way to reach it
+            # 2nd stair -- 2 ways to reach it
+            # by summing up the number of ways to reach the n-1 and n-2 stairs
+            # we can compute the total number of ways to climb the stairs
+
+
 
         prev = 1
-        prev2 = 0
+        curr = 1
 
-        for i in range(1, n+1):
-            curi = prev + prev2
-            prev2 = prev
-            prev = curi
-        return prev
+        for i in range(2, n+1):
+            temp = curr 
+            curr = prev + curr
+            prev = temp
+        return curr
