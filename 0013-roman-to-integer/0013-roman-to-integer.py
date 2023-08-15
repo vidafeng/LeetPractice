a8bcff374ee9bc615b88a4841090e8bfc4641ccf
vidafeng <71468356+vidafeng@users.noreply.github.com>
2023-08-14 21:57:18 -0400

    Time: 51 ms (86.38%), Space: 16.3 MB (80.15%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,10 +1,27 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        roman={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
-        number=0
-        for i in range(len(s)-1):
-            if roman[s[i]] < roman[s[(i+1)]]:
-                number-=roman[s[i]]
-            else:
-                number+=roman[s[i]]
-        return number+roman[s[-1]]
+        # when a smaller value appears before a larger value --> sub
+        # when smaller value appears after larger value --> add
+
+        hash = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+
+        res = 0
+
+        for i in range(len(s)):
+            if i < len(s) - 1 and hash[s[i]] < hash[s[i+1]]:
+                res -= hash[s[i]]
+            else: 
+                res += hash[s[i]]
+
+        return res
+
+
+
