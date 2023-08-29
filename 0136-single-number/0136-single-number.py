991a3fee8c0bc84018c4ed5898debd9680e705f0
vidafeng <71468356+vidafeng@users.noreply.github.com>
2023-08-28 23:24:37 -0400

    Time: 2807 ms (12.87%), Space: 19.4 MB (6.84%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,13 +1,17 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        hash = {}
+        # hash = {}
 
-        for num in nums:
-            if num in hash:
-                hash[num] += 1
-            else:
-                hash[num] = 1
+        # for num in nums:
+        #     if num in hash:
+        #         hash[num] += 1
+        #     else:
+        #         hash[num] = 1
 
-        for num in hash:
-            if hash[num] == 1:
-                return num
+        # for num in hash:
+        #     if hash[num] == 1:
+        #         return num
+
+        for i in set(nums):
+            if nums.count(i) == 1:
+                return i
