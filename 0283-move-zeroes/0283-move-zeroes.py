634aff639acb46627603ba90898a5babbbb1c6d3
vidafeng <71468356+vidafeng@users.noreply.github.com>
2023-08-29 23:59:45 -0400

    Time: 145 ms (80.3%), Space: 18 MB (46.82%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,17 +3,28 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # iterate through the list
-        # move all non zero elements to the begininf of the list
-        index = 0
+        slow = 0
+        for fast in range(len(nums)):
+            if nums[fast] != 0 and nums[slow] == 0:
+                nums[slow], nums[fast] = nums[fast], nums[slow]
 
-        # Place non-zero elements at the start of the list
-        for num in nums:
-            if num != 0:
-                nums[index] = num
-                index += 1
+            # wait while we find a non-zero element to
+            # swap with you
+            if nums[slow] != 0:
+                slow += 1
 
-        # Fill the remaining positions with zeroes
-        while index < len(nums):
-            nums[index] = 0
-            index += 1
+
+        # # iterate through the list
+        # # move all non zero elements to the begininf of the list
+        # index = 0
+
+        # # Place non-zero elements at the start of the list
+        # for num in nums:
+        #     if num != 0:
+        #         nums[index] = num
+        #         index += 1
+
+        # # Fill the remaining positions with zeroes
+        # while index < len(nums):
+        #     nums[index] = 0
+        #     index += 1
