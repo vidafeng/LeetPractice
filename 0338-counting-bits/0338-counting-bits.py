238fdcd580bba78d19a0cd59d0a340cbb3cdd837
vidafeng <71468356+vidafeng@users.noreply.github.com>
2023-09-06 00:00:14 -0400

    Time: 73 ms (67.25%), Space: 23.1 MB (22.55%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,5 +1,8 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
+        # use the number of 1's in i >>
+        # >> right shift operator
+
         ans = [0] * (n + 1)
         for i in range(1, n + 1):
             ans[i] = ans[i >> 1] + (i & 1)
