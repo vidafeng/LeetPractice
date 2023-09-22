2c5e22028389f82e6b96245df6a5157e2aa10627
vidafeng <71468356+vidafeng@users.noreply.github.com>
2023-09-22 11:06:17 -0400

    Time: 118 ms (20.2%), Space: 16.4 MB (39.71%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,32 +1,33 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        # left and right pointer
-        # keep track of seen chars, longest length 
-        # loop through the string
-            # need index!!!
-            # if char is in seen
-              # update left pointer to the index + 1 of same char (look up)
+        # Iterate through string using two pointers
+        # Use charSet to keep track of unique characters in current substring 
+        # Iterate through string using the right pointer
+        # If current character is not in charSet we have a unique char
+        # Insert the current char into the set and update max length
+        # Else move the left pointer to right pointer location
+        # Remove characters from set
+        # Insert current character into set and continue iteration
+        # Return maxLength
+        n = len(s)
+        maxLength = 0
+        currentLength = 0
+        charSet = set()
+        left = 0
 
-         # if char is NOT seen
-             # calculate length and compare to prev longest length 
-          # update seen to current index
-            # return length 
-
-        pointer = 0
-
-        seen = {}
-        longestLength = 0
-
-        for i in range(len(s)):
-            char = s[i]
-
-            if char in seen and seen[char] >= pointer:
-                pointer = seen[char] + 1
-            else:
-                longestLength = max(i-pointer + 1, longestLength)
-
-            seen[char] = i
-
-        return longestLength
+        for right in range(n):
+            if s[right] not in charSet:
+                charSet.add(s[right])
+                print("if", charSet)
+                maxLength = max(maxLength, right - left + 1)
+                print("maxl", maxLength)
+            else: 
+                while s[right] in charSet:
+                    charSet.remove(s[left])
+                    left += 1
+                charSet.add(s[right])
 
 
+        return maxLength
+
+        
