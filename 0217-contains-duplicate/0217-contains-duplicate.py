686806bf20c62cf85b67700aa8bdfff637b3dcd1
vidafeng <71468356+vidafeng@users.noreply.github.com>
2023-11-21 16:42:24 -0500

    Time: 481 ms (43.67%), Space: 30.8 MB (85.16%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,16 +1,39 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        hash = {}
+# HASH MAP METHOD
+#         hash = {}
         
+#         for num in nums:
+#             if num in hash:
+#                 return True
+#             else:
+#                 hash[num] = 1
+        
+#         return False
+
+# HASH SET METHOD o(n)
+# hash data structure to store encountered elements
+# iterate thru the array
+# if element is not in set, add
+# otherwise, return True
+        seen = set()
+
         for num in nums:
-            if num in hash:
+            if num in seen:
                 return True
-            else:
-                hash[num] = 1
-        
+            seen.add(num)
+
         return False
-        
 
 
-        
-        
+
+# BRUTE FORCE METHOD - time till exceed
+# compare each element with every other element in the array for duplicates
+# time complexity is o(n^2), not efficient for large arrays
+
+        # n = len(nums)
+        # for i in range(n - 1):
+        #     for j in range(i + 1, n):
+        #         if nums[i] == nums[j]:
+        #             return True
+        # return False
