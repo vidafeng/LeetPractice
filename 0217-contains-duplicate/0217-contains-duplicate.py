b3e855c0dee408ff626171e9d9576fe139b15101
vidafeng <71468356+vidafeng@users.noreply.github.com>
2023-11-21 16:32:15 -0500

    Time: 477 ms (55.05%), Space: 33.7 MB (7.89%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,44 +1,16 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        hash = {}
         
-# BRUTE FORCE -- time limit exceed
-# comparing every num with the other nums in the arr
-# o (n^2) time bc we are visiting every element in the arr twice
-
-#         n = len(nums)
-    
-#         for i in range(0, n-1):
-#             for j in range(i+1, n):
-#                 if nums[i] == nums[j]:
-#                     return True
-#         return False
-
-# SORT
-# sort the num arr and then loop
-# if i is equal to i-1, then we have a duplicate
-        
-#         nums.sort()
-#         n = len(nums)
-        
-#         for i in range(1, n):
-#             if nums[i] == nums[i-1]:
-#                 return True
-            
-#         return False
-
-# HASH SET
-# create a set
-# loop through nums arr and see if current num is in the set
-
-        hashSet = set()
-        print(hashSet)
         for num in nums:
-            if num in hashSet:
+            if num in hash:
                 return True
-            else: 
-                hashSet.add(num)
-
+            else:
+                hash[num] = 1
+        
         return False
-            
-    
+        
+
+
+        
         
