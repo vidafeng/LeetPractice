ffa7818b95883fdfff4a6c2b9b3860f0d01fdbad
vidafeng <71468356+vidafeng@users.noreply.github.com>
2023-12-05 16:06:38 -0500

    Time: 51 ms (54.16%), Space: 17.1 MB (50.23%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,20 +1,41 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        # convert all char in s to lowercase
-        # initialize empty result string
-        # iterate over given string by converting to lowercase
-        # check if corresponding value is character or num
-            # if it is, add to result string
-        # check if result string is palidrone by slicing from end
+# change all letters to lowercase
+# remove spaces and punctuation
 
-        res = ''
-        s = s.lower()
+# two pointer strategy
+# left pointer at index 0
+# right pointer at end of string
+# check characters at index l if they are either letters or nums
+# if not, increment
+# check characters at index r if they are either letters or nums 
+# if not, decrement
+# if characters are letters or nums, check against each other
+# if not the same, return false
 
-        for char in s:
-            if char.isalnum():
-                res += char
-        print(res)
+        l = 0
+        r = len(s) - 1
         
-        return res == res[::-1]
-         
+
+        while l < r: 
+            if not s[l].isalnum():
+                l += 1
+            elif not s[r].isalnum():
+                r -= 1
+            elif  s[l].lower() == s[r].lower():
+                l += 1
+                r -= 1
+            else:
+                return False
+            
+        return True
+            
+            
+            
+        
+        
+        
+
+    
+        
         
